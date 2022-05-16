ac0fae4668abcbe910f74e9baab4920f9bb55202
Thomas Simonini <simonini.thomas.pro@gmail.com>
2022-05-17 01:12:00 +0200

    Test

--- enjoy.py
+++ enjoy.py
@@ -75,10 +75,6 @@ def main():  # noqa: C901
     algo = args.algo
     folder = args.folder
 
-    if args.exp_id == 0:
-        args.exp_id = get_latest_run_id(os.path.join(folder, algo), env_id)
-        print(f"Loading latest experiment, id={args.exp_id}")
-
     # If model_id is defined it means we want to load from HF
     if args.model_id:
         # Define the destination path {folder}/{algo}/{env}_{exp_id}
@@ -92,6 +88,10 @@ def main():  # noqa: C901
         # repo_name = args.model_id.split("/")[1]
         # destination = os.path.join(args.folder, repo_name)
 
+    if args.exp_id == 0:
+        args.exp_id = get_latest_run_id(os.path.join(folder, algo), env_id)
+        print(f"Loading latest experiment, id={args.exp_id}")
+        
     # Sanity checks
     if args.exp_id > 0:
         log_path = os.path.join(folder, algo, f"{env_id}_{args.exp_id}")
